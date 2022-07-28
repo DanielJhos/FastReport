d5335369f42c3ff59248594601de2b75f0b024b3
Daniel <daniel.jhonata@outlook.com>
2022-07-28 20:47:52 -0300

    FastReport

---
+++ FastReport.dpr
@@ -0,0 +1,14 @@
+program FastReport;
+
+uses
+  Vcl.Forms,
+  uRelatorio in 'uRelatorio.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
